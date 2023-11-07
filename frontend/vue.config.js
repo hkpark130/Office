96ecbb62a4302bb3ce5cbec3a00228e18f2c37f5
park <park>
2023-11-07 12:09:10 +0900

    first init

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  filenameHashing: false, // build 파일 해싱 제거
+  productionSourceMap: true, // js source map 생성
+  css: {sourceMap: true}    // css source map 생성
 })
